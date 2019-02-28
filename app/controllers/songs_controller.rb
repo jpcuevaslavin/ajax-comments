f79fa5fe36e3cb250824b91fa2825d2eeefb40b3
Juan Pablo Cuevas <jp.cuevaslavin@gmail.com>
2019-02-28 11:19:29 -0300

    adds songs ajaxeable scaffold

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,6 +23,7 @@ class SongsController < ApplicationController
 
   # GET /songs/1/edit
   def edit
+    respond_to :js
   end
 
   # POST /songs
@@ -57,10 +58,7 @@ class SongsController < ApplicationController
   # DELETE /songs/1.json
   def destroy
     @song.destroy
-    respond_to do |format|
-      format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_to :js
   end
 
   private
